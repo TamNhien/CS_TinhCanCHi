d037f5a281a1a1fc2827e92a2af7cdfbfc4da301
TamNhien <152607459+TamNhien@users.noreply.github.com>
2024-06-10 19:11:10 +0700

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -17,137 +17,34 @@ namespace CS_TinhCanCHi
             InitializeComponent();
         }
 
-        string tinhcan(int nam)
+        string TinhCan(int nam)
         {
-            string can1 = "";
-            switch (nam % 10)
-            {
-                case 0:
-                    can1 = "Canh";
-                    break;
-                case 1:
-                    can1 = "Tân";
-                    break;
-                case 2:
-                    can1 = "Nhâm";
-                    break;
-                case 3:
-                    can1 = "Quý";
-                    break;
-                case 4:
-                    can1 = "Giáp";
-                    break;
-                case 5:
-                    can1 = "Ất";
-                    break;
-                case 6:
-                    can1 = "Bính";
-                    break;
-                case 7:
-                    can1 = "Đinh";
-                    break;
-                case 8:
-                    can1 = "Mậu";
-                    break;
-                case 9:
-                    can1 = "Kỵ";
-                    break;
-            }
-            return can1;
+            string[] canArray = { "Canh", "Tân", "Nhâm", "Quý", "Giáp", "Ất", "Bính", "Đinh", "Mậu", "Kỵ" };
+            return canArray[nam % 10];
+        }
+
+        string TinhChi(int nam)
+        {
+            string[] chiArray = { "Thân", "Dậu", "Tuất", "Hợi", "Tý", "Sửu", "Dần", "Mão", "Thìn", "Tỵ", "Ngọ", "Mùi" };
+            return chiArray[nam % 12];
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string chi = "", can = "";
             for (int i = 2000; i <= 2050; i++)
             {
-                //int indexCan = i % 10;
-                //int indexChi = i % 12;
-                lbCanChi.Items.Add(i.ToString()+ "-" +tinhcan(i));
+                lbCanChi.Items.Add($"{i} - {TinhCan(i)} {TinhChi(i)}");
             }
-            
         }
 
         private void btnTinh_Click(object sender, EventArgs e)
         {
-            string chi = "", can = "";
+            int nam = Convert.ToInt32(txtNam.Text);
+            string can = TinhCan(nam);
+            string chi = TinhChi(nam);
 
-            switch (Convert.ToInt32(txtNam.Text) % 10)
-            {
-                case 0:
-                    can = "Canh";
-                    break;
-                case 1:
-                    can = "Tân";
-                    break;
-                case 2:
-                    can = "Nhâm";
-                    break;
-                case 3:
-                    can = "Quý";
-                    break;
-                case 4:
-                    can = "Giáp";
-                    break;
-                case 5:
-                    can = "Ất";
-                    break;
-                case 6:
-                    can = "Bính";
-                    break;
-                case 7:
-                    can = "Đinh";
-                    break;
-                case 8:
-                    can = "Mậu";
-                    break;
-                case 9:
-                    can = "Kỵ";
-                    break;
-            }
-            switch (Convert.ToInt32(txtNam.Text) % 12)
-            {
-                case 0:
-                    chi = "Thân";
-                    break;
-                case 1:
-                    chi = "Dậu";
-                    break;
-                case 2:
-                    chi = "Tuất";
-                    break;
-                case 3:
-                    chi = "Hợi";
-                    break;
-                case 4:
-                    chi = "Tý";
-                    break;
-                case 5:
-                    chi = "Sửu";
-                    break;
-                case 6:
-                    chi = "Dần";
-                    break;
-                case 7:
-                    chi = "Mão";
-                    break;
-                case 8:
-                    chi = "Thìn";
-                    break;
-                case 9:
-                    chi = "Tỵ";
-                    break;
-                case 10:
-                    chi = "Ngọ";
-                    break;
-                case 11:
-                    chi = "Mùi";
-                    break;
-            }
-            txtCanChi.Text = can + " " + chi;
-
-            
-            lbCanChi.Items.Add(txtCanChi.Text);
+            string canChi = $"{can} {chi}";
+            txtCanChi.Text = canChi;
         }
     }
 }
